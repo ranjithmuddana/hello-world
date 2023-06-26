ec2bf128fc618e15449f00541bd84e48b880b314
Ranjith Muddana <25493833+ranjithmuddana@users.noreply.github.com>
2023-06-25 23:40:07 -0400

    Update hello.js

--- hello.js
+++ hello.js
@@ -10,24 +10,38 @@ pipeline {
             steps {
                 script {
                     def array2D = [
-                        ["Name", "Age", "Country"],
-                        ["Alice", "25", "USA"],
-                        ["Bob", "30", "Canada"],
-                        ["Charlie", "35", "USA"]
+                        ["Name", "Age", "Country", "Item4", "Item5", "Item6", "Item7"],
+                        ["Alice", "25", "USA", "Value4", "Value5", "Value6", "Value7"],
+                        ["Bob", "30", "Canada", "Value4", "Value5", "Value6", "Value7"],
+                        ["Charlie", "35", "USA", "Value4", "Value5", "Value6", "Value7"]
                     ]
                     
                     def emailTable = """
+                    <style>
+                    table {
+                        border-collapse: collapse;
+                        width: 100%;
+                    }
+                    
+                    th, td {
+                        border: 1px solid black;
+                        padding: 8px;
+                    }
+                    
+                    tr:nth-child(even) {
+                        background-color: #f2f2f2;
+                    }
+                    </style>
+                    
                     <table>
                       <thead>
                         <tr>
-                          <th>${array2D[0][0]}</th>
-                          <th>${array2D[0][1]}</th>
-                          <th>${array2D[0][2]}</th>
+                          <th>${array2D[0].join('</th><th>')}</th>
                         </tr>
                       </thead>
                       <tbody>
                         ${array2D[1..-1].collect { row ->
-                            "<tr><td>${row[0]}</td><td>${row[1]}</td><td>${row[2]}</td></tr>"
+                            "<tr><td>${row.join('</td><td>')}</td></tr>"
                         }.join('\n')}
                       </tbody>
                     </table>
