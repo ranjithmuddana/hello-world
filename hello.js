97ad068711adaf6c46aa6b12cc9f41ceeeb9b11a
Ranjith Muddana <25493833+ranjithmuddana@users.noreply.github.com>
2023-06-25 23:24:43 -0400

    Update hello.js

--- hello.js
+++ hello.js
@@ -2,55 +2,52 @@
 console.log("Hello, World!")
 
 
-def tableData = [
-    ["Name", "Age", "City"],
-    ["John", "30", "New York"],
-    ["Lisa", "25", "Los Angeles"],
-    ["Mike", "35", "Chicago"]
-]
-
-def formattedTable = ""
-tableData.each { row ->
-    formattedTable += row.join("|") + "|\n"
-}
-
-def message = """
-{
-  "text": "Table Example",
-  "cards": [
-    {
-      "sections": [
-        {
-          "widgets": [
-            {
-              "textParagraph": {
-                "text": "$formattedTable"
-              }
+pipeline {
+    agent any
+    
+    stages {
+        stage('Send Email') {
+            steps {
+                script {
+                    def array2D = [
+                        ["Name", "Age", "Country"],
+                        ["Alice", "25", "USA"],
+                        ["Bob", "30", "Canada"],
+                        ["Charlie", "35", "USA"]
+                    ]
+                    
+                    def emailTable = """
+                    <table>
+                      <thead>
+                        <tr>
+                          <th>${array2D[0][0]}</th>
+                          <th>${array2D[0][1]}</th>
+                          <th>${array2D[0][2]}</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        ${array2D[1..-1].collect { row ->
+                            "<tr><td>${row[0]}</td><td>${row[1]}</td><td>${row[2]}</td></tr>"
+                        }.join('\n')}
+                      </tbody>
+                    </table>
+                    """
+                    
+                    emailext (
+                        subject: 'Build Status: ${currentBuild.currentResult}',
+                        body: """<p>Hi,</p>
+                                 <p>The build status is: ${currentBuild.currentResult}</p>
+                                 <p>Here is the table:</p>
+                                 ${emailTable}
+                                 <p>Regards,</p>
+                                 <p>Jenkins</p>""",
+                        to: 'recipient@example.com',
+                        from: 'sender@example.com',
+                        replyTo: 'replyto@example.com',
+                        mimeType: 'text/html'
+                    )
+                }
             }
-          ]
-        }
-      ]
-    }
-  ],
-  "thread": {
-    "name": "MyThread"
-  }
-}
-"""
-
-def sendGoogleChatNotification(String webhookUrl, String message) {
-    def httpBuilder = new groovyx.net.http.HTTPBuilder(webhookUrl)
-    httpBuilder.request(Method.POST, groovyx.net.http.ContentType.JSON) {
-        body = message
-        response.success = { resp, reader ->
-            println("Google Chat notification sent successfully.")
-        }
-        response.failure = { resp ->
-            println("Failed to send Google Chat notification: ${resp.statusLine}")
         }
     }
 }
-
-// Call the function to send the Google Chat notification
-def webhookUrl = 'your-webhook-url'
-sendGoogleChatNotification(webhookUrl, message)
