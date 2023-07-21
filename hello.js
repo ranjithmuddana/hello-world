94c3386096905620006b0e9bbcddd69a07704a75
Ranjith Muddana <25493833+ranjithmuddana@users.noreply.github.com>
2023-07-21 10:24:46 -0400

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,3 +1,4 @@
+import com.google.api.gax.core.RetrySettings;
 import com.google.api.gax.paging.Page;
 import com.google.cloud.logging.Logging;
 import com.google.cloud.logging.LoggingOptions;
@@ -5,8 +6,6 @@ import com.google.cloud.logging.LogEntry;
 import com.google.cloud.logging.LogEntryListOption;
 import com.google.cloud.logging.Logger;
 import com.google.cloud.logging.Severity;
-import com.google.cloud.logging.v2.LoggingClient;
-import com.google.cloud.logging.v2.LoggingSettings;
 import com.google.common.collect.Lists;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +19,26 @@ public class GcpReadLogsService {
 
     public GcpReadLogsService() throws IOException {
         this.project = "your-project-id"; // Replace with your GCP project ID
-        this.logging = LoggingOptions.newBuilder().setRetrySettings(LoggingSettings.defaultRetrySettings()).build().getService();
+        this.logging = createLoggingClientWithRetrySettings();
+    }
+
+    private Logging createLoggingClientWithRetrySettings() {
+        RetrySettings retrySettings = RetrySettings.newBuilder()
+                .setInitialRetryDelayMillis(1000) // 1 second initial delay
+                .setMaxRetryDelayMillis(30000)    // 30 seconds maximum delay
+                .setTotalTimeoutMillis(600000)    // 10 minutes total timeout
+                .setInitialRpcTimeoutMillis(5000) // 5 seconds initial RPC timeout
+                .setMaxRpcTimeoutMillis(30000)    // 30 seconds maximum RPC timeout
+                .setRetryDelayMultiplier(1.3)     // Exponential backoff multiplier
+                .setRpcTimeoutMultiplier(1.5)     // RPC timeout multiplier
+                .setMaxAttempts(5)                // Maximum number of retry attempts
+                .build();
+
+        LoggingOptions loggingOptions = LoggingOptions.newBuilder()
+                .setRetrySettings(retrySettings)
+                .build();
+
+        return loggingOptions.getService();
     }
 
     public List<LogEntry> readLogs(String logResource, String filterStr, LoggingOptions options) {
