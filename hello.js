1e765cd0b643164663b67f94bddce203b1a545fa
Ranjith Muddana <25493833+ranjithmuddana@users.noreply.github.com>
2023-07-21 10:15:41 -0400

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,26 +1,17 @@
-import com.google.api.gax.retrying.RetrySettings;
-import com.google.cloud.logging.LogEntry;
-import com.google.cloud.logging.LogEntryListOption;
+import com.google.cloud.logging.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 public class GoogleCloudLoggingServiceTest {
 
     private static final String PROJECT_ID = "your-project-id";
-
-    // The @InjectMocks annotation injects the GoogleCloudLoggingService into the class under test
     private GoogleCloudLoggingService loggingService;
-
-    // The @Mock annotation creates a mock object for the Google Cloud Logging client library
     private Logging mockLogging;
 
     @BeforeEach
@@ -28,93 +19,43 @@ public class GoogleCloudLoggingServiceTest {
         // Create the mock object for Logging
         mockLogging = mock(Logging.class);
 
-        // Create the GoogleCloudLoggingService instance with default retry settings
-        loggingService = new GoogleCloudLoggingService(PROJECT_ID, mockLogging);
+        // Create the GoogleCloudLoggingService instance
+        loggingService = new GoogleCloudLoggingService(PROJECT_ID);
     }
 
     @Test
-    public void testListLogEntries_DefaultRetrySettings() {
+    public void testListLogEntries() {
         // Create a list of log entries to be returned by the mock Logging
         List<LogEntry> expectedLogEntries = Arrays.asList(
-                new LogEntry(),
-                new LogEntry()
+                LogEntry.newBuilder(StringPayload.of("Log entry 1")).build(),
+                LogEntry.newBuilder(StringPayload.of("Log entry 2")).build()
         );
 
         // Simulate a successful response without any exceptions
         when(mockLogging.listLogEntries(any(), any()))
                 .thenReturn(expectedLogEntries);
 
-        // Call the method to be tested
-        List<LogEntry> logEntries = loggingService.listLogEntries("projects/your-project-id/logs/your-log-name",
-                LogEntryListOption.filter("severity=ERROR"),
-                LogEntryListOption.option(LogEntryListOption.OptionType.DESCENDING));
+        // Replace "your-log-name" with the actual name of the log you want to read
+        String logName = "projects/your-project-id/logs/your-log-name";
+
+        // Define the filtering options if needed
+        LogEntryListOption[] options = {
+                LogEntryListOption.filter("severity=ERROR"), // Filter by severity if needed
+                LogEntryListOption.option(LogEntryListOption.OptionType.DESCENDING) // Sort by descending order if needed
+        };
+
+        // Create the GoogleCloudLoggingService instance with the mock Logging
+        loggingService = new GoogleCloudLoggingService(PROJECT_ID);
+        // Set the mock Logging object in the GoogleCloudLoggingService instance
+        ((GoogleCloudLoggingService) loggingService).setLoggingClient(mockLogging);
+
+        // Call the method to read the log entries
+        List<LogEntry> logEntries = loggingService.listLogEntries(logName, options);
+
+        // Verify that the method was called once
+        verify(mockLogging, times(1)).listLogEntries(eq(logName), eq(options));
 
         // Assert that the method returns the expected log entries
         assertEquals(expectedLogEntries, logEntries);
-    }
-
-    @Test
-    public void testListLogEntries_CustomRetrySettings() {
-        // Create a list of log entries to be returned by the mock Logging
-        List<LogEntry> expectedLogEntries = Arrays.asList(
-                new LogEntry(),
-                new LogEntry()
-        );
-
-        // Create custom retry settings
-        RetrySettings retrySettings = RetrySettings.newBuilder()
-                .setTotalTimeout(TimeUnit.SECONDS.toMillis(15))
-                .setMaxRetryDelay(TimeUnit.SECONDS.toMillis(5))
-                .setRetryableCodes(Arrays.asList(Code.UNAVAILABLE, Code.DEADLINE_EXCEEDED))
-                .build();
-
-        // Create a new GoogleCloudLoggingService instance with custom retry settings
-        loggingService = new GoogleCloudLoggingService(PROJECT_ID, retrySettings, mockLogging);
-
-        // Simulate an exception on the first invocation and a successful response on the second invocation
-        when(mockLogging.listLogEntries(any(), any()))
-                .thenThrow(new RuntimeException("Service unavailable"))
-                .thenReturn(expectedLogEntries);
-
-        // Call the method to be tested
-        List<LogEntry> logEntries = loggingService.listLogEntries("projects/your-project-id/logs/your-log-name",
-                LogEntryListOption.filter("severity=ERROR"),
-                LogEntryListOption.option(LogEntryListOption.OptionType.DESCENDING));
-
-        // Verify that the method retries two times (initial invocation + one retry)
-        verify(mockLogging, times(2)).listLogEntries(any(), any());
-
-        // Assert that the method returns the expected log entries after successful retry
-        assertEquals(expectedLogEntries, logEntries);
-    }
-
-    @Test
-    public void testListLogEntries_CustomRetrySettings_Failure() {
-        // Create custom retry settings with shorter timeout
-        RetrySettings retrySettings = RetrySettings.newBuilder()
-                .setTotalTimeout(TimeUnit.SECONDS.toMillis(5))
-                .setMaxRetryDelay(TimeUnit.SECONDS.toMillis(2))
-                .setRetryableCodes(Arrays.asList(Code.UNAVAILABLE, Code.DEADLINE_EXCEEDED))
-                .build();
-
-        // Create a new GoogleCloudLoggingService instance with custom retry settings
-        loggingService = new GoogleCloudLoggingService(PROJECT_ID, retrySettings, mockLogging);
-
-        // Simulate an exception for all retry attempts
-        when(mockLogging.listLogEntries(any(), any()))
-                .thenThrow(new RuntimeException("Service unavailable"))
-                .thenThrow(new RuntimeException("Service temporarily unavailable"))
-                .thenThrow(new RuntimeException("Service still unavailable"));
-
-        // Call the method to be tested
-        List<LogEntry> logEntries = loggingService.listLogEntries("projects/your-project-id/logs/your-log-name",
-                LogEntryListOption.filter("severity=ERROR"),
-                LogEntryListOption.option(LogEntryListOption.OptionType.DESCENDING));
-
-        // Verify that the method retries three times (initial invocation + two retries)
-        verify(mockLogging, times(3)).listLogEntries(any(), any());
-
-        // Assert that the method returns an empty list after exhausting all retries
-        assertEquals(Collections.emptyList(), logEntries);
     }
 }
