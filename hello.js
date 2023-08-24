2b65cc4a1901ed074094dc261289faeaabb6dab5
Ranjith Muddana <25493833+ranjithmuddana@users.noreply.github.com>
2023-08-24 03:42:15 -0400

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,53 +1,59 @@
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.util.retry.Retry;
+import com.google.auth.Credentials;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.*;
 
-import java.time.Duration;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.List;
+import java.util.Random;
 
-// ...
+public class GCSFilePicker {
+    public static void main(String[] args) {
+        String bucketName = "your-bucket-name"; // Replace with your GCS bucket name
+        List<Blob> unprocessedFiles = getUnprocessedFiles(bucketName);
 
-@Autowired
-private WebClient.Builder webClientBuilder;
+        if (!unprocessedFiles.isEmpty()) {
+            Random random = new Random();
+            int randomIndex = random.nextInt(unprocessedFiles.size());
+            Blob randomUnprocessedFile = unprocessedFiles.get(randomIndex);
 
-@Bean
-public IntegrationFlow messageFlow() {
-    return IntegrationFlows.from(pubsubInputChannel())
-            .handle(new MessageHandler() {
-                @Override
-                public void handleMessage(org.springframework.messaging.Message<?> message) throws MessagingException {
-                    try {
-                        // Process the Pub/Sub message here
-                        System.out.println("Received message: " + message.getPayload());
+            String lockFileName = randomUnprocessedFile.getName() + ".lock";
+            Blob lockFile = randomUnprocessedFile.getBucket().get(lockFileName);
 
-                        // Acknowledge the message immediately
-                        AcknowledgablePubsubMessage originalMessage =
-                                (AcknowledgablePubsubMessage) message.getPayload();
-                        originalMessage.ack();
-                        System.out.println("Acknowledged message");
+            if (lockFile == null) {
+                // Process the file
+                System.out.println("Processing file: " + randomUnprocessedFile.getName());
 
-                        // Send the message to another service using WebClient with retries (using block)
-                        boolean success = webClientBuilder.build()
-                                .post()
-                                .uri("http://other-service-url")
-                                .bodyValue(message.getPayload())
-                                .retrieve()
-                                .bodyToMono(Void.class)
-                                .retryWhen(Retry.fixedDelay(3, Duration.ofSeconds(20))) // Retry 3 times with 20 seconds delay
-                                .onErrorResume(error -> {
-                                    // Handle error here, e.g., log the error
-                                    System.err.println("Error sending message: " + error.getMessage());
-                                    return Mono.empty(); // Continue processing
-                                })
-                                .block(); // Synchronously block and wait for result
+                // Create lock file to mark as being processed
+                randomUnprocessedFile.getBucket().create(lockFileName, new byte[0], BlobTargetOption.doesNotExist());
 
-                        if (!success) {
-                            // Handle the case where retry attempts were exhausted
-                            System.err.println("Message delivery failed after retries.");
-                        }
-                    } catch (Exception e) {
-                        // Handle any exceptions that occur during processing
-                        e.printStackTrace();
-                    }
+                // Simulate processing
+                try {
+                    Thread.sleep(5000); // Simulating 5 seconds of processing
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
-            })
-            .get();
+
+                // Delete the lock file
+                lockFile.delete();
+            } else {
+                System.out.println("File is being processed by another instance.");
+            }
+        } else {
+            System.out.println("No unprocessed files available.");
+        }
+    }
+
+    private static List<Blob> getUnprocessedFiles(String bucketName) {
+        try {
+            Credentials credentials = GoogleCredentials.fromStream(new FileInputStream("path-to-your-credentials.json"));
+            Storage storage = StorageOptions.newBuilder().setCredentials(credentials).build().getService();
+
+            Bucket bucket = storage.get(bucketName);
+            return bucket.list(BlobListOption.currentDirectory(), BlobListOption.prefix("")).getValues();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
