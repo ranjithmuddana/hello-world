673a0008536a85e0c68f986594212cdd470fd71a
Ranjith Muddana <25493833+ranjithmuddana@users.noreply.github.com>
2023-08-26 15:26:45 -0400

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,59 +1,74 @@
-import com.google.auth.Credentials;
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.storage.*;
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.io.FileIO;
+import org.apache.beam.sdk.io.fs.ResolveOptions;
+import org.apache.beam.sdk.io.fs.ResourceId;
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.transforms.MapElements;
+import org.apache.beam.sdk.values.TypeDescriptor;
+import org.apache.beam.sdk.io.FileIO.Write.FileNaming;
+import org.apache.beam.sdk.io.FileIO;
+import org.apache.beam.sdk.io.fs.MatchResult.Metadata;
+import org.apache.beam.sdk.io.fs.ResourceId;
+import org.apache.beam.sdk.options.ValueProvider;
+import org.apache.beam.sdk.transforms.SimpleFunction;
+import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.values.TypeDescriptor;
+import java.util.Map;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.List;
-import java.util.Random;
+public class GCSMetadataFileIOWrite {
 
-public class GCSFilePicker {
     public static void main(String[] args) {
-        String bucketName = "your-bucket-name"; // Replace with your GCS bucket name
-        List<Blob> unprocessedFiles = getUnprocessedFiles(bucketName);
+        PipelineOptions options = PipelineOptionsFactory.create();
+        Pipeline pipeline = Pipeline.create(options);
 
-        if (!unprocessedFiles.isEmpty()) {
-            Random random = new Random();
-            int randomIndex = random.nextInt(unprocessedFiles.size());
-            Blob randomUnprocessedFile = unprocessedFiles.get(randomIndex);
+        PCollection<String> inputData = /* ... Your data source ... */;
 
-            String lockFileName = randomUnprocessedFile.getName() + ".lock";
-            Blob lockFile = randomUnprocessedFile.getBucket().get(lockFileName);
+        inputData.apply(FileIO.<String>write()
+            .to("gs://your-bucket/output")
+            .withNaming(new CustomFileNaming())
+            .via(new CustomFileIO.Sink()));
 
-            if (lockFile == null) {
-                // Process the file
-                System.out.println("Processing file: " + randomUnprocessedFile.getName());
+        pipeline.run().waitUntilFinish();
+    }
 
-                // Create lock file to mark as being processed
-                randomUnprocessedFile.getBucket().create(lockFileName, new byte[0], BlobTargetOption.doesNotExist());
-
-                // Simulate processing
-                try {
-                    Thread.sleep(5000); // Simulating 5 seconds of processing
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-
-                // Delete the lock file
-                lockFile.delete();
-            } else {
-                System.out.println("File is being processed by another instance.");
-            }
-        } else {
-            System.out.println("No unprocessed files available.");
+    static class CustomFileNaming implements FileNaming {
+        @Override
+        public ResourceId apply(ResourceId input, Context context) {
+            String outputName = context.windowedFilename();
+            return input.getCurrentDirectory().resolve(outputName, ResolveOptions.StandardResolveOptions.RESOLVE_FILE);
         }
     }
 
-    private static List<Blob> getUnprocessedFiles(String bucketName) {
-        try {
-            Credentials credentials = GoogleCredentials.fromStream(new FileInputStream("path-to-your-credentials.json"));
-            Storage storage = StorageOptions.newBuilder().setCredentials(credentials).build().getService();
+    static class CustomFileIO {
+        static class Sink implements FileIO.Sink<String> {
+            @Override
+            public void prepareWrite(WritableByteChannel channel) throws IOException {
+                // Custom preparations before writing
+            }
 
-            Bucket bucket = storage.get(bucketName);
-            return bucket.list(BlobListOption.currentDirectory(), BlobListOption.prefix("")).getValues();
-        } catch (IOException e) {
-            e.printStackTrace();
+            @Override
+            public void writeHeader(WritableByteChannel channel) throws IOException {
+                // Custom header writing if needed
+            }
+
+            @Override
+            public void writeFooter(WritableByteChannel channel) throws IOException {
+                // Custom footer writing if needed
+            }
+
+            @Override
+            public void writeRecord(String element, WritableByteChannel channel) throws IOException {
+                // Write the content of the element (metadata) to the channel
+                ByteBuffer buffer = ByteBuffer.wrap(element.getBytes(StandardCharsets.UTF_8));
+                channel.write(buffer);
+
+                // Set GCS metadata here
+                Map<String, String> metadata = ImmutableMap.of("your-metadata-key", "your-metadata-value");
+                GcsUtil gcsUtil = new GcsUtil();
+                GcsUtil.GcsMetadataUpdater updater = gcsUtil.createMetadataUpdater(metadata);
+                updater.update(new GcsPath(/* specify your output GCS path here */));
+            }
         }
-        return null;
     }
 }
