b2bf7baa659ea5599589d174566c809769f33264
Ranjith Muddana <25493833+ranjithmuddana@users.noreply.github.com>
2023-09-11 16:44:22 -0400

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,74 +1,26 @@
-import org.apache.beam.sdk.Pipeline;
-import org.apache.beam.sdk.io.FileIO;
-import org.apache.beam.sdk.io.fs.ResolveOptions;
-import org.apache.beam.sdk.io.fs.ResourceId;
-import org.apache.beam.sdk.options.PipelineOptions;
-import org.apache.beam.sdk.options.PipelineOptionsFactory;
-import org.apache.beam.sdk.transforms.MapElements;
-import org.apache.beam.sdk.values.TypeDescriptor;
-import org.apache.beam.sdk.io.FileIO.Write.FileNaming;
-import org.apache.beam.sdk.io.FileIO;
-import org.apache.beam.sdk.io.fs.MatchResult.Metadata;
-import org.apache.beam.sdk.io.fs.ResourceId;
-import org.apache.beam.sdk.options.ValueProvider;
-import org.apache.beam.sdk.transforms.SimpleFunction;
-import org.apache.beam.sdk.values.PCollection;
-import org.apache.beam.sdk.values.TypeDescriptor;
-import java.util.Map;
+import com.googlecode.juniversalchardet.UniversalDetector;
 
-public class GCSMetadataFileIOWrite {
-
+public class CharsetDetection {
     public static void main(String[] args) {
-        PipelineOptions options = PipelineOptionsFactory.create();
-        Pipeline pipeline = Pipeline.create(options);
+        byte[] data = /* Your byte array here */;
 
-        PCollection<String> inputData = /* ... Your data source ... */;
+        String detectedCharset = detectCharset(data);
 
-        inputData.apply(FileIO.<String>write()
-            .to("gs://your-bucket/output")
-            .withNaming(new CustomFileNaming())
-            .via(new CustomFileIO.Sink()));
-
-        pipeline.run().waitUntilFinish();
-    }
-
-    static class CustomFileNaming implements FileNaming {
-        @Override
-        public ResourceId apply(ResourceId input, Context context) {
-            String outputName = context.windowedFilename();
-            return input.getCurrentDirectory().resolve(outputName, ResolveOptions.StandardResolveOptions.RESOLVE_FILE);
+        if ("UTF-8".equals(detectedCharset)) {
+            System.out.println("Detected encoding: UTF-8");
+        } else if ("IBM1047".equals(detectedCharset)) {
+            System.out.println("Detected encoding: IBM1047 (CP1047)");
+        } else {
+            System.out.println("Unknown encoding");
         }
     }
 
-    static class CustomFileIO {
-        static class Sink implements FileIO.Sink<String> {
-            @Override
-            public void prepareWrite(WritableByteChannel channel) throws IOException {
-                // Custom preparations before writing
-            }
-
-            @Override
-            public void writeHeader(WritableByteChannel channel) throws IOException {
-                // Custom header writing if needed
-            }
-
-            @Override
-            public void writeFooter(WritableByteChannel channel) throws IOException {
-                // Custom footer writing if needed
-            }
-
-            @Override
-            public void writeRecord(String element, WritableByteChannel channel) throws IOException {
-                // Write the content of the element (metadata) to the channel
-                ByteBuffer buffer = ByteBuffer.wrap(element.getBytes(StandardCharsets.UTF_8));
-                channel.write(buffer);
-
-                // Set GCS metadata here
-                Map<String, String> metadata = ImmutableMap.of("your-metadata-key", "your-metadata-value");
-                GcsUtil gcsUtil = new GcsUtil();
-                GcsUtil.GcsMetadataUpdater updater = gcsUtil.createMetadataUpdater(metadata);
-                updater.update(new GcsPath(/* specify your output GCS path here */));
-            }
-        }
+    private static String detectCharset(byte[] data) {
+        UniversalDetector detector = new UniversalDetector(null);
+        detector.handleData(data, 0, data.length);
+        detector.dataEnd();
+        String charset = detector.getDetectedCharset();
+        detector.reset();
+        return charset;
     }
 }
