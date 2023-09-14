7664aa1f916fa06bf5f65530bcc59e4570fd22a4
Ranjith Muddana <25493833+ranjithmuddana@users.noreply.github.com>
2023-09-14 12:09:54 -0400

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,26 +1,35 @@
-import com.googlecode.juniversalchardet.UniversalDetector;
-
 public class CharsetDetection {
     public static void main(String[] args) {
         byte[] data = /* Your byte array here */;
 
-        String detectedCharset = detectCharset(data);
-
-        if ("UTF-8".equals(detectedCharset)) {
-            System.out.println("Detected encoding: UTF-8");
-        } else if ("IBM1047".equals(detectedCharset)) {
+        if (isIBM1047(data)) {
             System.out.println("Detected encoding: IBM1047 (CP1047)");
         } else {
             System.out.println("Unknown encoding");
         }
     }
 
-    private static String detectCharset(byte[] data) {
-        UniversalDetector detector = new UniversalDetector(null);
-        detector.handleData(data, 0, data.length);
-        detector.dataEnd();
-        String charset = detector.getDetectedCharset();
-        detector.reset();
-        return charset;
+    private static boolean isIBM1047(byte[] data) {
+        // Common EBCDIC control characters and patterns for IBM1047 (CP1047) encoding
+        byte[] commonEBCDICBytes = {
+            (byte) 0x0A, // Newline
+            (byte) 0x25, // Line feed
+            (byte) 0x3C, // Greater than or equal to
+            // Add more EBCDIC control characters or patterns as needed
+        };
+
+        for (byte currentByte : data) {
+            boolean isCommonEBCDIC = false;
+            for (byte commonEBCDICByte : commonEBCDICBytes) {
+                if (currentByte == commonEBCDICByte) {
+                    isCommonEBCDIC = true;
+                    break;
+                }
+            }
+            if (!isCommonEBCDIC) {
+                return false; // If any byte is not a common EBCDIC character, it's not IBM1047
+            }
+        }
+        return true; // All bytes match common EBCDIC characters, likely IBM1047
     }
 }
