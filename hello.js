017e05364fbe087c5925736f1fa57e53a04eccda
Ranjith Muddana <25493833+ranjithmuddana@users.noreply.github.com>
2023-09-22 01:46:20 -0400

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,35 +1,15 @@
-public class CharsetDetection {
-    public static void main(String[] args) {
-        byte[] data = /* Your byte array here */;
+#!/bin/bash
 
-        if (isIBM1047(data)) {
-            System.out.println("Detected encoding: IBM1047 (CP1047)");
-        } else {
-            System.out.println("Unknown encoding");
-        }
-    }
+# Specify the source file and destination directory
+source_file="gs://your-bucket/path/to/part-r-00003.dat"
+destination_directory="gs://your-bucket/path/to/destination/"
 
-    private static boolean isIBM1047(byte[] data) {
-        // Common EBCDIC control characters and patterns for IBM1047 (CP1047) encoding
-        byte[] commonEBCDICBytes = {
-            (byte) 0x0A, // Newline
-            (byte) 0x25, // Line feed
-            (byte) 0x3C, // Greater than or equal to
-            // Add more EBCDIC control characters or patterns as needed
-        };
+# Specify the number of copies you want
+num_copies=10
 
-        for (byte currentByte : data) {
-            boolean isCommonEBCDIC = false;
-            for (byte commonEBCDICByte : commonEBCDICBytes) {
-                if (currentByte == commonEBCDICByte) {
-                    isCommonEBCDIC = true;
-                    break;
-                }
-            }
-            if (!isCommonEBCDIC) {
-                return false; // If any byte is not a common EBCDIC character, it's not IBM1047
-            }
-        }
-        return true; // All bytes match common EBCDIC characters, likely IBM1047
-    }
-}
+# Loop to create and copy files with incremented numbers
+for ((i=1; i<=$num_copies; i++)); do
+  incremented_number=$(printf "%05d" $i)
+  destination_file="${destination_directory}part-r-${incremented_number}.dat"
+  gsutil cp "$source_file" "$destination_file"
+done
