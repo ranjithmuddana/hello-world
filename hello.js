5cafb92e594a84d52b110a4f54ab1c896b986052
Ranjith Muddana <25493833+ranjithmuddana@users.noreply.github.com>
2024-02-15 19:52:04 -0500

    Update hello.js

--- hello.js
+++ hello.js
@@ -8,5 +8,5 @@ WHERE
     condition;
 """
 
-table_names = re.findall(r'(?i)FROM\s+([a-z_][a-z0-9_$]*)', sql_query, re.MULTILINE)
+table_names = re.findall(r'(?i)FROM\s+([a-z_][a-z0-9_$]*)\.([a-z_][a-z0-9_$]*)', sql_query, re.MULTILINE)
 print(table_names)
