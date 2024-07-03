304b6ca2edc2eda9ba655067a6859d6230fcede3
Ranjith Muddana <25493833+ranjithmuddana@users.noreply.github.com>
2024-07-03 17:34:57 -0400

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,119 +1,20 @@
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.userinfo.OidcUserService;
-import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
-import org.springframework.security.oauth2.core.oidc.user.OidcUser;
-import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
-import org.springframework.session.web.http.HttpSessionEventPublisher;
+function Invoke-GsutilCommand {
+    param (
+        [string]$SubCommand,
+        [Parameter(ValueFromRemainingArguments = $true)]
+        $Arguments
+    )
 
-import javax.servlet.http.HttpSessionEvent;
-import javax.servlet.http.HttpSessionListener;
-import java.util.HashSet;
-import java.util.Set;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-            .authorizeRequests(authorizeRequests ->
-                authorizeRequests
-                    .antMatchers("/", "/login", "/custom/callback").permitAll()
-                    .anyRequest().authenticated()
-            )
-            .oauth2Login(oauth2Login ->
-                oauth2Login
-                    .loginPage("/login")
-                    .defaultSuccessUrl("/defaultSuccessUrl")
-                    .failureUrl("/login?error=true")
-                    .authorizationEndpoint(authorizationEndpoint ->
-                        authorizationEndpoint
-                            .baseUri("/oauth2/authorization")
-                            .authorizationRequestResolver(
-                                customAuthorizationRequestResolver(clientRegistrationRepository())
-                            )
-                    )
-                    .redirectionEndpoint(redirectionEndpoint ->
-                        redirectionEndpoint
-                            .baseUri("/custom/callback")
-                    )
-                    .userInfoEndpoint(userInfoEndpoint ->
-                        userInfoEndpoint
-                            .oidcUserService(this.oidcUserService())
-                    )
-            )
-            .sessionManagement()
-                .invalidSessionUrl("/session-timeout")
-                .maximumSessions(1)
-                .expiredUrl("/session-expired");
-    }
-
-    private DefaultOAuth2AuthorizationRequestResolver customAuthorizationRequestResolver(
-            ClientRegistrationRepository clientRegistrationRepository) {
-        DefaultOAuth2AuthorizationRequestResolver defaultResolver =
-            new DefaultOAuth2AuthorizationRequestResolver(
-                clientRegistrationRepository, "/oauth2/authorization");
-        return (request, clientRegistrationId) -> {
-            OAuth2AuthorizationRequest authorizationRequest = defaultResolver.resolve(request, clientRegistrationId);
-            if (authorizationRequest != null) {
-                return OAuth2AuthorizationRequest.from(authorizationRequest)
-                        .additionalParameters(params -> params.put("custom_param", "value"))
-                        .build();
-            }
-            return authorizationRequest;
-        };
-    }
-
-    @Bean
-    public OidcUserService oidcUserService() {
-        OidcUserService delegate = new OidcUserService();
-        return userRequest -> {
-            OidcUser oidcUser = delegate.loadUser(userRequest);
-            Set<GrantedAuthority> mappedAuthorities = new HashSet<>();
-
-            // Map the authorities
-            oidcUser.getAuthorities().forEach(authority -> {
-                if (authority instanceof OidcUserAuthority) {
-                    OidcUserAuthority oidcUserAuthority = (OidcUserAuthority) authority;
-                    // Extract roles from oidcUserAuthority and map to your roles if needed
-                }
-                mappedAuthorities.add(authority);
-            });
-
-            // Create a new user principal
-            return new DefaultOidcUser(mappedAuthorities, oidcUser.getIdToken(), oidcUser.getUserInfo());
-        };
-    }
-
-    @Bean
-    public SimpleUrlAuthenticationSuccessHandler successHandler() {
-        return new SimpleUrlAuthenticationSuccessHandler("/defaultSuccessUrl");
-    }
-
-    @Bean
-    public HttpSessionListener httpSessionListener() {
-        return new HttpSessionListener() {
-            @Override
-            public void sessionCreated(HttpSessionEvent se) {
-                se.getSession().setMaxInactiveInterval(30 * 60); // Set session timeout to 30 minutes
-            }
-
-            @Override
-            public void sessionDestroyed(HttpSessionEvent se) {
-                // Do something when session is destroyed, if needed
-            }
-        };
-    }
-
-    @Bean
-    public HttpSessionEventPublisher httpSessionEventPublisher() {
-        return new HttpSessionEventPublisher();
+    switch ($SubCommand) {
+        "ls" { & gsutil ls @Arguments }
+        "cat" { & gsutil cat @Arguments }
+        default { Write-Error "Unknown gsutil command: $SubCommand" }
     }
 }
+
+Set-Alias -Name gls -Value Invoke-GsutilCommand
+Set-Alias -Name gcat -Value Invoke-GsutilCommand
+
+# Example usage (uncomment to test within the script):
+# gls ls gs://my-bucket
+# gcat cat gs://my-bucket/my-file.txt
