3510b88779a52a7e4aa480db13735b17bb6a78fc
Ranjith Muddana <25493833+ranjithmuddana@users.noreply.github.com>
2024-08-30 17:54:01 -0400

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,24 +1,20 @@
-val query = """
-  SELECT
-    COALESCE(t1.id, t2.id) AS id,
-    t1.name AS name_df1,
-    t2.name AS name_df2,
-    CASE
-      WHEN t1.name IS NOT NULL AND t2.name IS NULL THEN 'df1'
-      WHEN t1.name IS NULL AND t2.name IS NOT NULL THEN 'df2'
-      WHEN t1.name IS NOT NULL AND t1.name != t2.name THEN 'both'
-      ELSE 'no_diff'
-    END AS source
-  FROM
-    table1 t1
-  FULL OUTER JOIN
-    table2 t2
-  ON
-    t1.id = t2.id
-  WHERE
-    t1.name IS DISTINCT FROM t2.name
-"""
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.expressions.UserDefinedFunction
 
-val dfDiff = spark.sql(query)
-
-dfDiff.show()
+// Define the UDF
+val diffUdf: UserDefinedFunction = udf { (str1: String, str2: String) =>
+  if (str1 == null || str2 == null) {
+    null
+  } else {
+    val maxLength = math.max(str1.length, str2.length)
+    (0 until maxLength).map { i =>
+      if (i < str1.length && i < str2.length && str1(i) == str2(i)) {
+        str1(i)
+      } else if (i < str1.length || i < str2.length) {
+        '^'
+      } else {
+        ""
+      }
+    }.mkString
+  }
+}
