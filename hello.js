fc83cef6b73fc7274d168042a6ce78c68523196a
Ranjith Muddana <25493833+ranjithmuddana@users.noreply.github.com>
2024-08-30 17:38:48 -0400

    Update hello.js

--- hello.js
+++ hello.js
@@ -2,7 +2,13 @@ val query = """
   SELECT
     COALESCE(t1.id, t2.id) AS id,
     t1.name AS name_df1,
-    t2.name AS name_df2
+    t2.name AS name_df2,
+    CASE
+      WHEN t1.name IS NOT NULL AND t2.name IS NULL THEN 'df1'
+      WHEN t1.name IS NULL AND t2.name IS NOT NULL THEN 'df2'
+      WHEN t1.name IS NOT NULL AND t1.name != t2.name THEN 'both'
+      ELSE 'no_diff'
+    END AS source
   FROM
     table1 t1
   FULL OUTER JOIN
