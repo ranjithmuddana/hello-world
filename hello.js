86de238c65bbd9f603aceba993276a32305e447c
Ranjith Muddana <25493833+ranjithmuddana@users.noreply.github.com>
2024-08-31 11:39:07 -0400

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,29 +1,7 @@
-import org.apache.spark.sql.functions._
-import org.apache.spark.sql.expressions.UserDefinedFunction
+val joinedDF = df1.withColumn("source", lit("df1"))
+  .union(df2.withColumn("source", lit("df2")))
+  .groupBy(df1.columns.map(col): _*)
+  .agg(collect_set("source").as("sources"))
+  .filter(size(col("sources")) === 1)
 
-// Define the enhanced UDF
-val csvDiffUdf: UserDefinedFunction = udf { (str1: String, str2: String) =>
-  if (str1 == null || str2 == null) {
-    null
-  } else {
-    val list1 = str1.split(",").map(_.trim)
-    val list2 = str2.split(",").map(_.trim)
-    val maxLength = math.max(list1.length, list2.length)
-
-    val diffs = (0 until maxLength).flatMap { i =>
-      if (i < list1.length && i < list2.length) {
-        if (list1(i) != list2(i)) {
-          Some(s"${list1(i)}->${list2(i)}")
-        } else {
-          None
-        }
-      } else if (i < list1.length) {
-        Some(s"${list1(i)}->null")
-      } else {
-        Some(s"null->${list2(i)}")
-      }
-    }
-
-    if (diffs.isEmpty) "no_diff" else diffs.mkString(", ")
-  }
-}
+joinedDF.show()
