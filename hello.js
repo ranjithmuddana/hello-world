861bf03fddf95b675476a4dee47c4372c3c606b9
Ranjith Muddana <25493833+ranjithmuddana@users.noreply.github.com>
2024-09-06 08:49:22 -0400

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,9 +1,23 @@
-SELECT 
-    ASSERT_TRUE(
-        COUNT(*) = 0,
-        'There are rows that do not have exactly 3 pipes'
-    )
-FROM 
-    your_table
-WHERE 
-    LENGTH(col) - LENGTH(REPLACE(col, '|', '')) != 3;
+import org.apache.spark.sql.functions.udf
+import scala.xml.{Elem, XML}
+
+// Function to convert XML element to a fixed format string
+def xmlElementToFixed(element: scala.xml.Node): String = {
+  val elementName = element.label
+  val text = element.text.trim
+  val attributes = element.attributes.asAttrMap.map { case (key, value) => s"$key=$value" }.mkString(", ")
+  
+  val children = element.child.filter(_.isInstanceOf[Elem]).map(xmlElementToFixed).mkString(" | ")
+  
+  s"Element: $elementName, Text: $text, Attributes: [$attributes], Children: [$children]"
+}
+
+// Define the UDF
+val xmlToFixed = udf((xmlStr: String) => {
+  try {
+    val root = XML.loadString(xmlStr)
+    xmlElementToFixed(root)
+  } catch {
+    case _: Exception => null
+  }
+})
