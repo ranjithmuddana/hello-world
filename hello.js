e3bb5fb51de93089204d6e101414f22fb1cc704f
Ranjith Muddana <25493833+ranjithmuddana@users.noreply.github.com>
2024-09-17 11:49:00 -0400

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,16 +1,8 @@
 ConnectionFactory connectionFactory = ConnectionFactories.get(
-    ConnectionFactoryOptions.builder()
-        .option(ConnectionFactoryOptions.DRIVER, "pool")
-        .option(ConnectionFactoryOptions.PROTOCOL, "postgresql")  // Change based on your DB
-        .option(ConnectionFactoryOptions.HOST, "your-db-host")
-        .option(ConnectionFactoryOptions.USER, "your-username")
-        .option(ConnectionFactoryOptions.PASSWORD, "your-password")
-        .option(ConnectionFactoryOptions.DATABASE, "your-database")
-        // Pool settings
-        .option(ConnectionFactoryOptions.POOL_INITIAL_SIZE, 10)
-        .option(ConnectionFactoryOptions.POOL_MAX_SIZE, 50)
-        .option(ConnectionFactoryOptions.POOL_MAX_IDLE_TIME, Duration.ofMinutes(1))
-        .option(ConnectionFactoryOptions.POOL_MAX_LIFE_TIME, Duration.ofMinutes(30))
-        .option(ConnectionFactoryOptions.POOL_VALIDATION_QUERY, "SELECT 1")
-        .build()
+    "r2dbc:pool:postgresql://username:password@host:5432/database" +
+    "?maxSize=20" +
+    "&initialSize=10" +
+    "&validationQuery=SELECT 1" +
+    "&maxIdleTime=30s" +
+    "&maxLifeTime=30m"
 );
