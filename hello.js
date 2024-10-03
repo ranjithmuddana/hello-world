b3db7168191da81b4046774a919f80ef8e3e5395
Ranjith Muddana <25493833+ranjithmuddana@users.noreply.github.com>
2024-10-03 15:24:36 -0400

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,7 +1,3 @@
 SELECT 
-  LPAD('', 30, ' ') AS blank_col, -- Default to 30 blanks
-  OVERLAY(
-    OVERLAY(LPAD('', 30, ' ') PLACING SUBSTRING(name, 1, 10) FROM 1) -- Replace first 10 chars with 'name' substring
-    PLACING SUBSTRING(ssn, 20, 11) FROM 20 -- Replace from 20 to 30 with 'ssn' substring
-  ) AS result_col
+  LPAD(CAST(ROW_NUMBER() OVER (ORDER BY RAND()) AS STRING), 11, '0') AS unique_seq_number
 FROM your_table;
