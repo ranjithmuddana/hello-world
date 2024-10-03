17ce852e35db8aaa319c9afc96a007521abbcb32
Ranjith Muddana <25493833+ranjithmuddana@users.noreply.github.com>
2024-10-03 14:39:23 -0400

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,1 +1,7 @@
-.*\s+TimeTaken\s*=\s*(\d+)\s*$
+SELECT 
+  LPAD('', 30, ' ') AS blank_col, -- Default to 30 blanks
+  OVERLAY(
+    OVERLAY(LPAD('', 30, ' ') PLACING SUBSTRING(name, 1, 10) FROM 1) -- Replace first 10 chars with 'name' substring
+    PLACING SUBSTRING(ssn, 20, 11) FROM 20 -- Replace from 20 to 30 with 'ssn' substring
+  ) AS result_col
+FROM your_table;
